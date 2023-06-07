221c00fa792f7ad13b0c9f1d970ca26283ebe7e6
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-07 10:19:23 -0300

    refactors button variant

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+type Variant = 'contained' | 'outlined';
 
 @Component({
   selector: 'app-button',
@@ -7,4 +9,14 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class ButtonComponent {
   @Output() click: EventEmitter<MouseEvent> = new EventEmitter<MouseEvent>();
+
+  @Input() variant: Variant = 'contained';
+
+  styles = variants;
 }
+
+const variants = {
+  base: 'text-sm py-2 px-6 rounded-full flex justify-center items-center font-extrabold text-center',
+  contained: 'bg-primary-400 text-white',
+  outlined: 'bg-transparent text-primary-400 border border-primary-400',
+};
