8305c3b137335d8c59804eb2aced1b7c2a5eb9c6
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-07 01:45:53 -0300

    scaffolds navbar component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,11 +2,13 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
   declarations: [
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
 })
 export class ComponentsModule {}
