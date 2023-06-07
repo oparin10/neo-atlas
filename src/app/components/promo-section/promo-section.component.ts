8a82f3d94a579d4d23e10dd5d721c3afd75c85ae
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-07 07:46:20 -0300

    promo section impl

--- src/app/components/promo-section/promo-section.component.ts
+++ src/app/components/promo-section/promo-section.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Type } from '@angular/core';
+import { AngularIconSvgComponent } from 'src/app/svg/angular-icon-svg/angular-icon-svg.component';
+import { FigmaIconSvgComponent } from 'src/app/svg/figma-icon-svg/figma-icon-svg.component';
+import { JsIconSvgComponent } from 'src/app/svg/js-icon-svg/js-icon-svg.component';
+import { ReactIconSvgComponent } from 'src/app/svg/react-icon-svg/react-icon-svg.component';
+import { RustIconSvgComponent } from 'src/app/svg/rust-icon-svg/rust-icon-svg.component';
+import { TsIconSvgComponent } from 'src/app/svg/ts-icon-svg/ts-icon-svg.component';
+
+type SkillsComponents = {
+  id: number;
+  component: Type<any>;
+};
 
 @Component({
   selector: 'app-promo-section',
   templateUrl: './promo-section.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class PromoSectionComponent {
-
+  skills: SkillsComponents[] = [
+    { component: AngularIconSvgComponent, id: 0 },
+    { component: ReactIconSvgComponent, id: 1 },
+    { component: TsIconSvgComponent, id: 2 },
+    { component: JsIconSvgComponent, id: 3 },
+    { component: RustIconSvgComponent, id: 4 },
+    { component: FigmaIconSvgComponent, id: 5 },
+  ];
 }
