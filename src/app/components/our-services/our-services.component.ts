2e1817926d7d8968a03b536face90113c76bf844
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-07 08:27:49 -0300

    our services section impl

--- src/app/components/our-services/our-services.component.ts
+++ src/app/components/our-services/our-services.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { ServiceItem } from '../service-item/service-item.component';
+import { ServiceIconOneComponent } from 'src/app/svg/service-icon-one/service-icon-one.component';
+import { ServiceIconTwoComponent } from 'src/app/svg/service-icon-two/service-icon-two.component';
+import { ServiceIconThreeComponent } from 'src/app/svg/service-icon-three/service-icon-three.component';
+import { ServiceIconFourComponent } from 'src/app/svg/service-icon-four/service-icon-four.component';
 
 @Component({
   selector: 'app-our-services',
@@ -7,5 +11,26 @@ import { ServiceItem } from '../service-item/service-item.component';
   styles: [],
 })
 export class OurServicesComponent {
-  services: ServiceItem[] = [];
+  services: ServiceItem[] = [
+    {
+      icon: ServiceIconOneComponent,
+      title: 'Design thinking',
+      text: 'Planejamos e organizamos dinâmicas para discovery.',
+    },
+    {
+      icon: ServiceIconTwoComponent,
+      title: 'Design de Interfaces',
+      text: 'Criação de fluxos de telas com protótipos de alta fidelidade.',
+    },
+    {
+      icon: ServiceIconThreeComponent,
+      title: 'Análise Heurística',
+      text: 'Identificação de problemas, insights e dicas de otimização.',
+    },
+    {
+      icon: ServiceIconFourComponent,
+      title: 'Desenvolvimento Web',
+      text: 'Transforme suas ideias em soluções digitais de sucesso.',
+    },
+  ];
 }
