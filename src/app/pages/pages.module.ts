cba03d67dd55429a3bbc44afacbbd08df9d1192f
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-07 02:28:34 -0300

    Landing page module structure

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { LandingModule } from './landing/landing.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, LandingModule],
 })
-export class PagesModule { }
+export class PagesModule {}
