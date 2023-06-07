772f8116672599211956bab6a8becd037247fcf3
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-07 01:45:10 -0300

    scaffolds footer component

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,8 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
+  declarations: [
+    FooterComponent
+  ],
 })
 export class ComponentsModule {}
