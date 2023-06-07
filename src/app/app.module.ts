944074e8ccbffeaa2b2f1807b5f38bce8b5aa5c1
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-07 18:24:41 -0300

    removes obsolete modules from app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { LandingLayoutModule } from './layouts/landing-layout/landing-layout.module';
-import { ComponentsModule } from './components/components.module';
-import { SvgModule } from './svg/svg.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule,
-    LandingLayoutModule,
-    ComponentsModule,
-    SvgModule,
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, LandingLayoutModule],
   providers: [],
   bootstrap: [AppComponent],
 })
