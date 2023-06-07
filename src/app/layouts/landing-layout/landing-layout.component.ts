abdcda99796fb8437e1dcb1d4857e9f2612fc3f7
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-07 19:48:50 -0300

    adds bg pattern

--- src/app/layouts/landing-layout/landing-layout.component.ts
+++ src/app/layouts/landing-layout/landing-layout.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'landing-layout',
   templateUrl: './landing-layout.component.html',
+  styleUrls: ['./landing-layout.styles.css'],
 })
 export class LandingLayoutComponent implements OnInit {
   constructor() {}
