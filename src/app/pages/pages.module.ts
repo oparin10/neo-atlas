703e94d3352779eca1e480fb7442c4121da32e73
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-10 03:01:27 -0300

    scaffolds contact page

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ComponentsModule } from '../components/components.module';
 import { SvgModule } from '../svg/svg.module';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
-  declarations: [LandingPageComponent],
+  declarations: [LandingPageComponent, ContactComponent],
   imports: [CommonModule, ComponentsModule, SvgModule],
   exports: [LandingPageComponent],
 })
