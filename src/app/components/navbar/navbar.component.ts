ca59d01e451d5fdbbdc2af575d9f26b864bff7c8
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-12 01:31:55 -0300

    injects viewportService in navbar component to reactively update the button size based on the viewport

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Size as ButtonSize } from '../button/button.component';
+import { ViewportService } from '../../services/viewport.service';
 
 @Component({
   selector: 'app-navbar',
@@ -8,11 +9,15 @@ import { Size as ButtonSize } from '../button/button.component';
 export class NavbarComponent implements OnInit {
   buttonSize: ButtonSize = 'medium';
 
-  onResize() {
-    this.buttonSize = window.innerWidth < 768 ? 'small' : 'medium';
-  }
+  constructor(private readonly viewportService: ViewportService) {}
 
   ngOnInit(): void {
-    this.onResize();
+    this.viewportService.resize$.subscribe(() => {
+      this.updateButtonSize();
+    });
+  }
+
+  private updateButtonSize(): void {
+    this.buttonSize = window.innerWidth < 768 ? 'small' : 'medium';
   }
 }
