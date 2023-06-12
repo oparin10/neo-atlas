fe22318a2633274a7bdb379f973c54b19bbefc96
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-12 00:20:12 -0300

    process-card component refactor

--- src/app/components/our-process/our-process.component.ts
+++ src/app/components/our-process/our-process.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { ProcessCardComponentType } from '../process-card/process-card.component';
+import { ChatIconComponent } from '../../svg/chat-icon/chat-icon.component';
+import { PuzzleIconSvgComponent } from '../../svg/puzzle-icon-svg/puzzle-icon-svg.component';
+import { RocketIconSvgComponent } from '../../svg/rocket-icon-svg/rocket-icon-svg.component';
 
 @Component({
   selector: 'app-our-process',
   templateUrl: './our-process.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class OurProcessComponent {
-
+  items: ProcessCardComponentType[] = [
+    {
+      icon: ChatIconComponent,
+      text: 'Para descobrir o seu problema por meio de pesquisas e aprendizados.',
+      title: 'Conversa',
+    },
+    {
+      icon: PuzzleIconSvgComponent,
+      text: 'Para estruturar processos e solucionar o seu problema.',
+      title: 'Mapeamento',
+    },
+    {
+      icon: RocketIconSvgComponent,
+      text: 'Para transformar nossas ideias em realidade.',
+      title: 'Execução',
+    },
+  ];
 }
--- src/app/components/process-card/process-card.component.ts
+++ src/app/components/process-card/process-card.component.ts
@@ -1,11 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Type } from '@angular/core';
+import { RocketIconSvgComponent } from 'src/app/svg/rocket-icon-svg/rocket-icon-svg.component';
 
+export type ProcessCardComponentType = {
+  icon: Type<any>;
+  title: string;
+  text: string;
+};
 @Component({
   selector: 'app-process-card',
   templateUrl: './process-card.component.html',
   styles: [],
 })
 export class ProcessCardComponent {
+  @Input() icon: Type<any> = RocketIconSvgComponent;
   @Input() title: string = 'Placeholder';
   @Input() text: string = 'Lorem ipsum dolum salet schepazi daertirus';
 }
