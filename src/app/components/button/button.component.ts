83973109d92e0ff01401b9276db3317fce40af4f
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-12 00:34:40 -0300

    button styling enhancements

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -17,7 +17,8 @@ export class ButtonComponent {
 }
 
 const variants = {
-  base: 'w-full text-sm py-2 px-10 rounded-full flex justify-center items-center font-extrabold text-center',
-  contained: 'bg-primary-400 text-white',
-  outlined: 'bg-transparent text-primary-400 border border-primary-400',
+  base: 'active:scale-[0.95] transition-all w-full text-sm py-2 px-10 rounded-full flex justify-center items-center font-extrabold text-center',
+  contained: 'hover:bg-primary-500 bg-primary-400 text-white',
+  outlined:
+    'hover:border-primary-500 hover:text-primary-500 bg-transparent text-primary-400 border border-primary-400 hover:bg-primary-100/50',
 };
