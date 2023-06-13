a054d0af50c598aa0d1f75475d3ed01c3b88db1e
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-13 16:33:58 -0300

    refactors text-area to impl ControlValueAccessor interface so it can be used within a FormGroup

--- ui/src/app/components/text-area/text-area.component.ts
+++ ui/src/app/components/text-area/text-area.component.ts
@@ -1,10 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-text-area',
   templateUrl: './text-area.component.html',
   styles: [],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TextAreaComponent),
+      multi: true,
+    },
+  ],
 })
-export class TextAreaComponent {
+export class TextAreaComponent implements ControlValueAccessor {
   @Input() label?: string = 'Placeholder label';
+
+  value = '';
+
+  onChange: any = () => {};
+  onTouched: any = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(): void {}
+
+  setDisabledState(): void {}
 }
