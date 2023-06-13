11b5ea2d98dae823c6c6c084b0c4e6112d7061b9
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-13 17:53:39 -0300

    adds  validators to the contact form; implements function to mask the phone input; creates regex pattern to validate phone input

--- ui/src/app/components/contact-form/contact-form.component.ts
+++ ui/src/app/components/contact-form/contact-form.component.ts
@@ -1,5 +1,25 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+type ContactForm = {
+  name: string;
+  phone: string;
+  email: string;
+  message: string;
+};
+
+// regex pattern (99) 9-9999-9999
+const PATTERN = /^\(\d{2}\)\s\d-\d{4}-\d{4}$/;
+
+const maskInput = (string: string) => {
+  const maskedValue = string
+    .replace(/\D/g, '')
+    .slice(0, 11)
+    .replace(/^(\d{2})(\d)/g, '($1) $2')
+    .replace(/(\d)(\d{4})$/, '$1-$2');
+
+  return maskedValue;
+};
 
 @Component({
   selector: 'app-contact-form',
@@ -10,10 +30,21 @@ export class ContactFormComponent {
   @Output() formSubmitted = new EventEmitter<(typeof this.form)['value']>();
 
   form = new FormGroup({
-    name: new FormControl(''),
-    phone: new FormControl(''),
-    email: new FormControl(''),
-    message: new FormControl(''),
+    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.pattern(PATTERN),
+      Validators.minLength(15),
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(5),
+    ]),
+    message: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
   });
 
   onSubmit() {
