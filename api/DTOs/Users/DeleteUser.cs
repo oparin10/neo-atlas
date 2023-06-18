d46ae41968a22e128dd03dc2556d03dc6b6ec7c9
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-18 20:25:32 -0300

    adds validation to DeleteUser DTO

--- api/DTOs/Users/DeleteUser.cs
+++ api/DTOs/Users/DeleteUser.cs
@@ -1,6 +1,15 @@
-
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.DTOs.Users;
 
 
-public record DeleteUser(long Id);
+public record DeleteUser
+{
+    [Required(ErrorMessage = "User ID is required")]
+    public long Id { get; }
+
+    public DeleteUser(long id)
+    {
+        Id = id;
+    }
+}
