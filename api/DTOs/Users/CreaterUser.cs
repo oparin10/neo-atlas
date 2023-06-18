bc7e776f9957240caa34c6c2337ad5a4ed648225
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-18 20:44:01 -0300

    adds validation to CreateUser  DTO

--- api/DTOs/Users/CreaterUser.cs
+++ api/DTOs/Users/CreaterUser.cs
@@ -1,4 +1,25 @@
+using System.ComponentModel.DataAnnotations;
 
 namespace Api.DTOs.Users;
 
-public record CreateUser(string Username, string Password, string Email);
+public record CreateUser
+{
+    [Required(ErrorMessage = "Username is required")]
+    public readonly string Username;
+
+    [Required(ErrorMessage = "Password is required")]
+    [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
+    public readonly string Password;
+
+
+    [Required(ErrorMessage = "Email is required")]
+    [EmailAddress(ErrorMessage = "Email is not valid")]
+    public readonly string Email;
+
+    public CreateUser(string username, string password, string email)
+    {
+        Username = username;
+        Password = password;
+        Email = email;
+    }
+}
