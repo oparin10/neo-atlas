9732d510dfcdadc5d3f527c92962c89e68c151ad
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-18 19:23:04 -0300

    refactors WebsiteContext to inherit from BaseDbContext class

--- api/Context/WebsiteContext.cs
+++ api/Context/WebsiteContext.cs
@@ -4,25 +4,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Api.Context;
 
-public partial class WebsiteContext : DbContext
+public partial class WebsiteContext : BaseDbContext
 {
 
     public WebsiteContext(DbContextOptions<WebsiteContext> options) : base(options) { }
-
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseMySql("server=localhost;user=root;password=root;database=db", ServerVersion.Parse("11.0.2-mariadb"));
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder
-            .UseCollation("utf8mb4_general_ci")
-            .HasCharSet("utf8mb4");
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 
     public DbSet<ContactItem> Contacts { get; set; } = null!;
 }
