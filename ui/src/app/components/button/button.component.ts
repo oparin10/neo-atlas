cf949bc3cd9970e95e60cb75dbe872fe4aa2f3c2
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-19 03:00:30 -0300

    button extensions and more login form tweaks

--- ui/src/app/components/button/button.component.ts
+++ ui/src/app/components/button/button.component.ts
@@ -15,6 +15,7 @@ export class ButtonComponent {
   @Input() variant: Variant = 'contained';
   @Input() size: Size = 'medium';
   @Input() href?: string;
+  @Input() class?: string;
 
   styles = variants;
   sizes = sizes;
@@ -29,11 +30,11 @@ const states = {
 const sizes = {
   medium: 'text-sm py-2 px-10',
   small: 'text-[0.625rem] py-1 px-8',
-  large: 'text-lg py-2 px-12',
+  large: 'text-xl py-4 px-12',
 };
 
 const variants = {
-  base: 'active:scale-[0.95] transition-all w-full text-sm py-2 px-10 rounded-full flex justify-center items-center font-extrabold text-center',
+  base: 'active:scale-[0.95] transition-all w-full py-2 px-10 rounded-full flex justify-center items-center font-extrabold text-center',
   contained: 'hover:bg-primary-500 bg-primary-400 text-white',
   outlined:
     'hover:border-primary-500 hover:text-primary-500 bg-transparent text-primary-400 border border-primary-400 hover:bg-primary-100/50',
