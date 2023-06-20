8427e68579086e6b40d58dbaa5a878c444854a77
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-20 17:18:37 -0300

    active state styles

--- ui/src/app/components/text-field/text-field.component.ts
+++ ui/src/app/components/text-field/text-field.component.ts
@@ -12,11 +12,23 @@ export class TextFieldComponent {
 const styles_tw = {
   legend:
     'float-[unset] w-auto origin-top-left text-[8px] overflow-hidden block invisible h-[11px] duration-300 transition-[max-width_transform]',
-  fieldset:
-    'pl-3 text-left m-0 peer-focus:[&>legend]:max-w-lg [&>legend]:max-w-[0.01px] peer-focus:border-2 peer-focus:border-primary-400 overflow-hidden top-[-5px] border border-solid border-gray-300 pointer-events-none absolute inset-0 rounded-[inherit]',
-  label:
-    'pl-3 peer-focus:translate-x-[5px] peer-focus:text-[8px] duration-200 peer-focus:duration-300 cursor-text pointer-events-none text-gray-500 transition-all peer-focus:font-bold peer-focus:text-primary-400 h-fit select-none max-w-fit block overflow-hidden whitespace-nowrap overflow-ellipsis origin-top-left translate-y-[-50%] top-[50%] peer-focus:translate-y-[0%] peer-focus:top-[-25%] text-xs absolute inset-0',
 
   input: 'peer focus:ring-0 border-none w-full rounded-[inherit]',
+  label: {
+    base: 'top-[50%] translate-y-[-50%] left-0 pl-3 duration-200  text-gray-500 transition-all  h-fit origin-top-left text-xs absolute',
+    hasValue:
+      'peer-[&:not(:placeholder-shown)]:translate-y-[-30px] peer-[&:not(:placeholder-shown)]:text-[8px] peer-[&:not(:placeholder-shown)]:duration-300 peer-[&:not(:placeholder-shown)]:translate-x-[5px]',
+    focus:
+      'peer-focus:translate-x-[5px] peer-focus:translate-y-[-30px] peer-focus:text-[8px] peer-focus:duration-300 peer-focus:text-primary-400',
+    reset:
+      'overflow-ellipsis block overflow-hidden whitespace-nowrap max-w-fit select-none pointer-events-none cursor-text',
+  },
+  fieldset: {
+    hasValue: 'peer-[&:not(:placeholder-shown)]:[&>legend]:max-w-lg',
+    base: 'pl-3 [&>legend]:max-w-[0.01px] overflow-hidden top-[-5px] border border-solid border-gray-300 rounded-[inherit]',
+    reset: 'text-left m-0 absolute inset-0 overflow-hidden pointer-events-none',
+    focus:
+      'peer-focus:[&>legend]:max-w-lg peer-focus:border-2 peer-focus:border-primary-400',
+  },
   span: 'px-[5px] whitespace-nowrap',
 };
