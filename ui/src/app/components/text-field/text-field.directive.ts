b86f5cc66893e9d3dd10c72d80e5759a0399133f
Aleksander Mirowsky <alex.xande10@gmail.com>
2023-06-22 23:39:47 -0300

    TextFieldDirective functionality to automatically assign the TextField wrapper generate id to bind the label and the input

--- ui/src/app/components/text-field/text-field.directive.ts
+++ ui/src/app/components/text-field/text-field.directive.ts
@@ -1,6 +1,7 @@
 // inject styling as a directive to the text field component
 
-import { Directive } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, ViewChild } from '@angular/core';
+import { TextFieldWrapper } from './text-field-wrapper.component';
 
 @Directive({
   selector: 'input[textField]',
@@ -9,4 +10,17 @@ import { Directive } from '@angular/core';
       'peer focus:ring-0 border-none w-full rounded-[inherit] text-sm py-3',
   },
 })
-export class TextFieldDirective {}
+export class TextFieldDirective implements AfterViewInit {
+  constructor(
+    private elementRef: ElementRef<HTMLInputElement>,
+    private textFieldWrapper: TextFieldWrapper
+  ) {}
+
+  ngAfterViewInit(): void {
+    if (this.textFieldWrapper) {
+      const inputElement = this.elementRef.nativeElement;
+
+      inputElement.id = this.textFieldWrapper.inputId;
+    }
+  }
+}
